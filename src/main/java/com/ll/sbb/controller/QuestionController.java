f220fe4db8bb3b126cbc77f886ab20b8e180ddd9
nnWon <nwwn4627@gmail.com>
2023-11-21 17:47:31 +0900

    feat: 질문 상세 기능 추가
    - QuestionController에 detail() 메서드 추가
    - QuestionService에 getQuestion() 메서드 추가
    - 해당 질문이 없을 때 발생하는 DataNotFoundException 정의
    - 질문 상세 페이지 question-detail.html 작성

--- src/main/java/com/ll/sbb/controller/QuestionController.java
+++ src/main/java/com/ll/sbb/controller/QuestionController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -26,5 +27,12 @@ public class QuestionController {
         List<Question> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question-list";
+    }
+
+    @GetMapping("/question/detail/{questionId}")
+    public String detail(Model model,@PathVariable Integer questionId){
+        Question question = questionService.getQuestion(questionId);
+        model.addAttribute("question", question);
+        return "question-detail";
     }
 }
--- src/main/java/com/ll/sbb/service/QuestionService.java
+++ src/main/java/com/ll/sbb/service/QuestionService.java
@@ -2,10 +2,12 @@ package com.ll.sbb.service;
 
 import com.ll.sbb.domain.question.Question;
 import com.ll.sbb.domain.question.QuestionRepository;
+import com.ll.sbb.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +17,10 @@ public class QuestionService {
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer questionId){
+        Optional<Question> byId = questionRepository.findById(questionId);
+        return byId.orElseThrow(() ->  new DataNotFoundException("question not found"));
     }
 }
