84f3df4569a61c83cffb5e6cae497ad993c2a7d6
nnWon <nwwn4627@gmail.com>
2023-11-21 17:50:22 +0900

    update: QuestionController 클래스 레벨에 @RequestMapping 적용
    - 루트 URL을 담당하는 MainController 작성

--- src/main/java/com/ll/sbb/controller/QuestionController.java
+++ src/main/java/com/ll/sbb/controller/QuestionController.java
@@ -8,28 +8,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/question")
 @RequiredArgsConstructor
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/")
-    public String root(){
-        return "redirect:/question/list";
-    }
-
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question-list";
     }
 
-    @GetMapping("/question/detail/{questionId}")
+    @GetMapping("/detail/{questionId}")
     public String detail(Model model,@PathVariable Integer questionId){
         Question question = questionService.getQuestion(questionId);
         model.addAttribute("question", question);
