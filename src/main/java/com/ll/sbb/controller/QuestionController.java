17cae4f3ee7a7c8e3dcfa76ba15064cf79655b89
nnWon <nwwn4627@gmail.com>
2023-11-22 15:40:11 +0900

    feat: 질문 등록 Validation 구현

--- src/main/java/com/ll/sbb/controller/QuestionController.java
+++ src/main/java/com/ll/sbb/controller/QuestionController.java
@@ -1,12 +1,17 @@
 package com.ll.sbb.controller;
 
 import com.ll.sbb.domain.question.Question;
-import com.ll.sbb.domain.question.QuestionRepository;
+import com.ll.sbb.dto.QuestionForm;
 import com.ll.sbb.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -25,18 +30,21 @@ public class QuestionController {
     }
 
     @GetMapping("/create")
-    public String questionCreate() {
+    public String questionCreate(QuestionForm questionForm) {
         return "question-form";
     }
 
     @PostMapping("/create")
-    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
-        questionService.create(subject,content);
+    public String questionCreate(@Validated QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question-form";
+        }
+        questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list";
     }
 
     @GetMapping("/detail/{questionId}")
-    public String detail(Model model,@PathVariable Integer questionId){
+    public String detail(Model model, @PathVariable Integer questionId) {
         Question question = questionService.getQuestion(questionId);
         model.addAttribute("question", question);
         return "question-detail";
